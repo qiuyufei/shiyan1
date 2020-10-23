63ca9ed17c3286e3ff269681c9be370bafbd4aa0
qiuyufei <72742566+qiuyufei@users.noreply.github.com>
2020-10-23 15:06:30 +0800

    Update HardDisk.java

--- HardDisk.java
+++ HardDisk.java
@@ -2,11 +2,18 @@ package com;
 
 public class HardDisk {
 	private int amount;
+	private int A;
 	public int getAmount() {
 		return amount;
 	}
+	public int getA() {
+		return A;
+	}
 	public void setAmount(int amount) {
 		this.amount = amount;
+	}
+	public void setA(int A) {
+		this.A = A;
 	}
 
 }
