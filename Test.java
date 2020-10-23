f2f3a025a7c82869ad42586dc264e8a1b49efc20
qiuyufei <72742566+qiuyufei@users.noreply.github.com>
2020-10-23 12:30:20 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,9 +3,15 @@ package com;
 public class Test {
 	public static void main(String[] args) {
 		CPU cpu = new CPU();
+		
 		cpu.setSpeed(2200);
+		cpu.setSize(88);
+		
 		HardDisk disk = new HardDisk();
+		
 		disk.setAmount(200);
+		disk.setA(188);
+		
 		PC pc = new PC();
 		pc.setCpu(cpu);
 		pc.setDisk(disk);
