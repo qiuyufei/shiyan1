f5f51cc964ebcc103e81560ce180eb26cc8bc465
qiuyufei <72742566+qiuyufei@users.noreply.github.com>
2020-10-23 15:02:01 +0800

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -2,6 +2,13 @@ package com;
 
 public class CPU {
 	private int speed;
+	private String A;
+	public int getSpeed() {
+  		return speed;
+	}
+ 	public Sting getA() {
+		return A;
+	}
 	
 	public int getSpeed() {
 		return speed;
@@ -9,5 +16,8 @@ public class CPU {
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
+	public void setA(String A) {
+ 		this.A = A;
+}
 
 }
